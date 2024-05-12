c579bd6ab0dd1ece8a88c3d8b4a530e0c8960b53
Varun-MSI <tvarun2014@gmail.com>
2024-05-12 17:43:45 +0530

    New

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+// import HomePage from './Components/HomePage';
 import MissingLetterGame from './Components/MissingLetterGame';
 import './Components/MissingLetterGame.css';
 // import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 function App() {
   return (
     <>
-      <MissingLetterGame/>
+    {/* <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/game" element={<MissingLetterGame/>}/>
+      </Routes>
+    </Router> */}
+    <MissingLetterGame/>
     </>
   );
 }
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export default function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+    return (
+        <div>
+            <div class="card">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+    )
 }
