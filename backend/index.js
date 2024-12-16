91c42519a0a5b9d9f6b95133d4eb28313b98307f
VarunReddyT <tvarun2014@gmail.com>
2024-12-16 15:34:13 +0530

    Backend

--- backend/index.js
+++ backend/index.js
@@ -17,44 +17,6 @@ mongoose.connect('mongodb://localhost:27017/jwlgames')
         console.log("Error connecting to database");
     });
 
-app.post("/api/gameData/:gameId", async (req, res) => {
-    const levelData = {
-        words: [
-            {
-                level: 1,
-                words: [
-                    { word: "if", letters: ["p", "i", "d"] },
-                    { word: "at", letters: ["a", "b", "n"] },
-                    { word: "go", letters: ["f", "w", "g"] },
-                    { word: "be", letters: ["k", "x", "b"] },
-                    { word: "do", letters: ["p", "d", "c"] }
-                ]
-            },
-            {
-                level: 2,
-                words: [
-                    { word: "rid", letters: ["n", "y", "r"] },
-                    { word: "put", letters: ["p", "a", "s"] },
-                    { word: "rag", letters: ["p", "r", "d"] },
-                    { word: "led", letters: ["j", "g", "l"] },
-                    { word: "rug", letters: ["y", "r", "n"] }
-                ]
-            }
-        ]
-    };
-
-    const gameData = new game({
-        words: levelData.words
-    });
-    try {
-        await gameData.save();
-        res.status(201).send(gameData);
-    }
-    catch (err) {
-        res.status(400).send(err);
-    }
-});
-
 app.get('/api/getGameData/:gameId', async (req, res) => {
     try {
         const gameData = await game.findOne();
